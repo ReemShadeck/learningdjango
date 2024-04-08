83451a12bab6c0520df88e6241f05f2e30a0fca4
ReemS <140219097+ReemShadeck@users.noreply.github.com>
2024-04-07 23:09:42 -0700

    Update settings.py

--- learningdjango/settings.py
+++ learningdjango/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1-**v6r^r8*i_8%pvmoj58asq1kj!uk7=2ap$!e&6u(ik5v&sg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
 ### ###
